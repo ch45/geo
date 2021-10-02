e7100817aa0fa3253880c91b18146aeec5d5a197
chaz <ch45@live.co.uk>
2021-10-02 21:15:08 +0100

    Format a marker for highest environmental measurement. Be a bit more robust for the input file.

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 let csvData = 'http://' + location.hostname + ':8888/geo/data/gps_2021-09-15_Throop_Loop.csv';
 
 let dataTypes = [
-  {"include": false, "name": "Particulates <1um ug/m3", "column": "PMS 1.0", "colour": "#5946B2"},
-  {"include": false, "name": "Particulates <2.5um ug/m3", "column": "PMS 2.5", "colour": "#9C51B6"},
-  {"include": true, "name": "Particulates <10um ug/m3", "column": "PMS 10.0", "colour": "#E936A7"},
+  {"include": false, "name": "Particulates <1um", "column": "PMS 1.0", "colour": "#5946B2"},
+  {"include": false, "name": "Particulates <2.5um", "column": "PMS 2.5", "colour": "#9C51B6"},
+  {"include": true, "name": "Particulates <10um", "column": "PMS 10.0", "colour": "#E936A7"},
   {"include": false, "name": "Carbon Monoxide (CO)", "column": "Gas Reducing", "colour": "#8FD400"},
   {"include": true, "name": "Nitrogen Dioxide (NO2)", "column": "Gas Oxidizing", "colour": "#319177"},
   {"include": false, "name": "Ammonia (NH3)", "column": "Gas NH3", "colour": "#4A646C"},
   {"include": false, "name": "Noise Low", "column":"Noise Low", "colour": "#000000"},
-	{"include": false, "name": "Noise Mid", "column":"Noise Mid", "colour": "#000000"},
-	{"include": false, "name": "Noise High", "column":"Noise High", "colour": "#000000"},
-	{"include": false, "name": "Noise Total", "column":"Noise Total", "colour": "#000000"},
-	{"include": false, "name": "Temperature", "column":"Temperature", "colour": "#000000"},
-	{"include": false, "name": "Humidity", "column":"Humidity", "colour": "#000000"},
+  {"include": false, "name": "Noise Mid", "column":"Noise Mid", "colour": "#000000"},
+  {"include": false, "name": "Noise High", "column":"Noise High", "colour": "#000000"},
+  {"include": false, "name": "Noise Total", "column":"Noise Total", "colour": "#000000"},
+  {"include": false, "name": "Temperature", "column":"Temperature", "colour": "#000000"},
+  {"include": false, "name": "Humidity", "column":"Humidity", "colour": "#000000"},
   {"include": false, "name": "Light", "column":"Lux", "colour": "#000000"},
 ];
 
@@ -25,9 +25,6 @@ let myMap;
 let mappa = new Mappa('Leaflet');
 let needToFit = true;
 let mapChanged = false;
-let moveToMode = true;
-let moveToX;
-let moveToY;
 let legend;
 let selectionChanged = false;
 let selectLastId;
@@ -40,10 +37,10 @@ function preload() {
 
 
 function setup() {
-  let rows = table.getRowCount();
-  let cols = table.getColumnCount();
+  let numRows = table.getRowCount();
+  let numCols = table.getColumnCount();
 
-  print("csv data rows " + rows + " cols " + cols);
+  print("csv data numRows " + numRows + " numCols " + numCols);
 
   convertLatLong();
   extents = getExtents();
@@ -106,7 +103,7 @@ function draw() {
       }
       fill(red(dataTypes[j].colour), green(dataTypes[j].colour), blue(dataTypes[j].colour), blobAlpha);
       stroke(red(dataTypes[j].colour), green(dataTypes[j].colour), blue(dataTypes[j].colour), blobAlpha);
-      drawEnvironmental(dataTypes[j].column);
+      drawEnvironmental(dataTypes[j].column, dataTypes[j].name);
     }
 
     stroke(0, 0, 255);
@@ -130,10 +127,16 @@ function draw() {
 
 
 function drawTrack() {
-  let rows = table.getRowCount();
-  for (let r = 0; r < rows; r++) {
-    let ptLat = table.get(r, 0);
-    let ptLong = table.get(r, 2);
+  let latCol = colFromName("Latitude");
+  let longCol = colFromName("Longitude");
+  let numRows = table.getRowCount();
+  let moveToMode = true;
+  let moveToX;
+  let moveToY;
+
+  for (let r = 0; r < numRows; r++) {
+    let ptLat = table.get(r, latCol);
+    let ptLong = table.get(r, longCol);
     let pos = myMap.latLngToPixel(ptLat, ptLong);
     if (pos.x >= 0 && pos.x <= windowWidth && pos.y >= 0 && pos.y <= windowHeight) {
       noFill();
@@ -150,55 +153,65 @@ function drawTrack() {
       }
     }
   }
-  if (!moveToMode) {
-    moveToMode = true;
-  }
 }
 
 
-function drawEnvironmental(colName) {
+function drawEnvironmental(colName, enviroName) {
   let min;
-  let max = -1;
+  let max = -Number.MAX_VALUE;
   let maxRadius = blobMaxRadius;
+  let maxLat;
+  let maxLong;
+  let maxPtTime;
   let data = [];
-  let rows = table.getRowCount();
-  let col = colFromName(colName);
-  if (!col) {
+  let latCol = colFromName("Latitude");
+  let longCol = colFromName("Longitude");
+  let numRows = table.getRowCount();
+  let envCol = colFromName(colName);
+  if (!envCol) {
     return;
   }
-  for (let r = 0; r < rows; r++) {
-    let ptLat = table.get(r, 0);
-    let ptLong = table.get(r, 2);
-    let ptVal = parseFloat(table.get(r, col));
+  for (let r = 0; r < numRows; r++) {
+    let ptLat = table.get(r, latCol);
+    let ptLong = table.get(r, longCol);
+    let ptVal = parseFloat(table.get(r, envCol));
     let pos = myMap.latLngToPixel(ptLat, ptLong);
     if (pos.x >= 0 && pos.x <= windowWidth && pos.y >= 0 && pos.y <= windowHeight) {
       data.push([pos.x, pos.y, ptVal]);
       if (ptVal > max) {
-        if (max === -1) {
+        if (max === -Number.MAX_VALUE) {
           min = ptVal;
         }
         max = ptVal;
+        maxLat = ptLat;
+        maxLong = ptLong;
+        maxPtTime = table.getString(r, colFromName("Time"));
       }
       if (ptVal < min) {
         min = ptVal;
       }
     }
   }
-  let step = Math.trunc(data.length / (99 + col) + 1);
+  let step = Math.trunc(data.length / (99 + envCol) + 1);
   for (let j = 0; j < data.length; j += step) {
     let r = map(data[j][2], min, max, 0, maxRadius);
     ellipse(data[j][0], data[j][1], r, r);
   }
+  if (max > -1) {
+    var html = '<p>' + enviroName + '<br />' + fmtValueSigFig(max, 4) + '<br />' + fmtTime(maxPtTime) + '</p>';
+    var popup = L.popup().setLatLng([maxLat, maxLong]).setContent(html);
+    popup.addTo(myMap.map);
+  }
 }
 
 function colFromName(colName) {
-  let cols = table.getColumnCount();
-  for (var j = 0; j < cols; j++) {
+  let numCols = table.getColumnCount();
+  for (var j = 0; j < numCols; j++) {
     if (table.columns[j].trim() === colName) {
       break;
     }
   }
-  if (j === cols) {
+  if (j === numCols) {
     return;
   }
   return j;
@@ -216,58 +229,79 @@ function cbMapChanged() {
 
 
 function convertLatLong() {
-  let recentLat = 0;
-  let recentLong = 0;
-  let firstLat = 0;
-  let firstLong = 0;
-  let firstGPSRow = 0;
-  let rows = table.getRowCount();
+  let recentLat;
+  let recentLong;
+  let recentSet = false;
+  let firstLat;
+  let firstLong;
+  let firstGPSRow;
+  let firstSet = false;
+  let latCol = colFromName("Latitude");
+  let hemiCol = latCol + 1;
+  let longCol = colFromName("Longitude");
+  let sideCol = longCol + 1;
+  let numRows = table.getRowCount();
 
-  for (let r = 0; r < rows; r++) {
-    let inLat = table.getString(r, 0);
-    let inHemi = table.getString(r, 1);
-    let inLong = table.getString(r, 2);
-    let inSide = table.getString(r, 3);
-    if (inLat === "" && inLong === "") {
-      if (recentLat > 0 && recentLong > 0) {
-        table.set(r, 0, recentLat);
-        table.set(r, 2, recentLong);
+  for (let r = 0; r < numRows; r++) {
+    let ptLat = table.getString(r, latCol);
+    let ptHemi = table.getString(r, hemiCol);
+    let ptLong = table.getString(r, longCol);
+    let ptSide = table.getString(r, sideCol);
+    if (ptLat === "" && ptLong === "") {
+      if (recentSet) {
+        table.set(r, latCol, recentLat);
+        table.set(r, longCol, recentLong);
       }
     } else {
-      let outLat = Math.floor(inLat/100) + Math.floor(inLat % 100)/60 + (inLat-Math.floor(inLat))*60/3600;
-      if (inHemi === "S") {
-        outLat = -outLat;
+      let convertedLat = Math.floor(ptLat/100) + Math.floor(ptLat % 100)/60 + (ptLat-Math.floor(ptLat))*60/3600;
+      if (ptHemi === "S") {
+        convertedLat = -convertedLat;
       }
-      let outLong = Math.floor(inLong/100) + Math.floor(inLong % 100)/60 + (inLong-Math.floor(inLong))*60/3600;
-      if (inSide === "W") {
-        outLong = -outLong;
+      let convertedLong = Math.floor(ptLong/100) + Math.floor(ptLong % 100)/60 + (ptLong-Math.floor(ptLong))*60/3600;
+      if (ptSide === "W") {
+        convertedLong = -convertedLong;
       }
-      table.set(r, 0, outLat);
-      table.set(r, 2, outLong);
-      if (recentLat === 0 && recentLong === 0) {
-        recentLat = outLat;
-        recentLong = outLong;
+      table.set(r, latCol, convertedLat);
+      table.set(r, longCol, convertedLong);
+      if (!recentSet) {
+        recentSet = true;
+        recentLat = convertedLat;
+        recentLong = convertedLong;
+      }
+      if (!firstSet) {
+        firstSet = true;
+        firstLat = convertedLat;
+        firstLong = convertedLong;
         firstGPSRow = r;
       }
     }
   }
   for (let r = 0; r < firstGPSRow; r++) {
-    table.set(r, 0, firstLat);
-    table.set(r, 2, firstLong);
+    table.set(r, latCol, firstLat);
+    table.set(r, longCol, firstLong);
   }
 }
 
 
 function getExtents() {
-  let rows = table.getRowCount();
+  let latCol = colFromName("Latitude");
+  let longCol = colFromName("Longitude");
+  let numRows = table.getRowCount();
   let leastLat = 90;
   let leastLong = 180;
   let greatestLat = -90;
   let greatestLong = -180;
 
-  for (let r = 0; r < rows; r++) {
-    let ptLat = table.get(r, 0);
-    let ptLong = table.get(r, 2);
+  let ptLat = table.get(0, latCol);
+  let ptLong = table.get(0, longCol);
+  leastLat = ptLat;
+  leastLong = ptLong;
+  greatestLat = ptLat;
+  greatestLong = ptLong;
+
+  for (let r = 0; r < numRows; r++) {
+    ptLat = table.get(r, latCol);
+    ptLong = table.get(r, longCol);
     if (ptLat < leastLat) {
       leastLat = ptLat;
     }
@@ -293,4 +327,14 @@ function getCentre(extents) {
 
 function makeId(x) {
   return x.replace(' ', '-');
+}
+
+
+function fmtValueSigFig(num, sigFig) {
+  return num.toLocaleString(undefined, { maximumSignificantDigits: sigFig });
+}
+
+
+function fmtTime(time) {
+  return time.substring(0, 2) + ':' + time.substring(2, 4) + ':' + time.substring(4, 6);
 }
